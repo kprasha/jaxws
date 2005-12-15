bf74e6711d462f462f5c5cc77ec4e299d992267e
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-12-15 00:58:46 +0000

    slowly making progress.

--- jaxws-ri/rt/src/com/sun/xml/ws/sandbox/message/impl/jaxb/JAXBMessage.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/sandbox/message/impl/jaxb/JAXBMessage.java
@@ -1,7 +1,51 @@
 package com.sun.xml.ws.sandbox.message.impl.jaxb;
 
+import com.sun.xml.ws.sandbox.message.Message;
+import com.sun.xml.ws.sandbox.message.HeaderList;
+import com.sun.xml.ws.sandbox.message.MessageProperties;
+import com.sun.xml.ws.sandbox.message.AttachmentSet;
+
 /**
+ * {@link Message} backed by a JAXB bean.
+ *
  * @author Kohsuke Kawaguchi
  */
-public class JAXBMessage {
+public /*for now, work in progress*/ abstract class JAXBMessage extends Message {
+    private HeaderList headers;
+    private final MessageProperties props;
+
+    public JAXBMessage() {
+        props = new MessageProperties();
+    }
+
+    /**
+     * Copy constructor.
+     */
+    public JAXBMessage(JAXBMessage that) {
+        this.headers = that.headers;
+        if(this.headers!=null)
+            this.headers = new HeaderList(this.headers);
+        // TODO: do we need to clone this? I guess so.
+        this.props = that.props;
+    }
+
+    public HeaderList getHeaders() {
+        if(headers==null)
+            headers = new HeaderList();
+        return headers;
+    }
+
+    public MessageProperties getProperties() {
+        return props;
+    }
+
+    public AttachmentSet getAttachments() {
+        return AttachmentSet.EMPTY;
+    }
+
+    public Message copy() {
+        return new JAXBMessage(this);
+    }
+
+
 }
