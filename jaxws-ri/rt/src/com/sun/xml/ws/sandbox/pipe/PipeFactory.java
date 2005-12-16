20acd2559a707b980708cd6633551861c7d3c353
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-12-16 23:23:40 +0000

    this was half-baked anyway.

--- jaxws-ri/rt/src/com/sun/xml/ws/sandbox/pipe/PipeFactory.java
+++
@@ -1,9 +0,0 @@
-package com.sun.xml.ws.sandbox.pipe;
-
-/**
- * @author Kohsuke Kawaguchi
- */
-public interface PipeFactory {
-    // TODO: think about what would be the input
-    Pipe create( Object/* TODO: change it to Model, or maybe a new 'Policy' */ model );
-}
