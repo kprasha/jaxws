5d1a44fadf25cdab45325d2ba7be374a20c64d1e
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-01-03 23:00:21 +0000

    added note.

---
+++ jaxws-ri/rt/src/com/sun/xml/ws/sandbox/message/impl/jaxb/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * {@link Message} implementation for JAXB.
+ *
+ * <pre>
+ * TODO:
+ *      Because a producer of a message doesn't generally know
+ *      when a message is consumed, it's difficult for
+ *      the caller to do a proper instance caching. Perhaps
+ *      there should be a layer around JAXBContext that does that?
+ * </pre>
+ */
+package com.sun.xml.ws.sandbox.message.impl.jaxb;
+
+import com.sun.xml.ws.sandbox.message.Message;
