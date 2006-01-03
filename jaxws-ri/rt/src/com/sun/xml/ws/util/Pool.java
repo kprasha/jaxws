2a4fdf61f8ed4cf6c7138a2e0ca74b18997efec1
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-01-03 19:39:06 +0000

    added a general purpose pool implmentation.

---
+++ jaxws-ri/rt/src/com/sun/xml/ws/util/Pool.java
@@ -0,0 +1,59 @@
+package com.sun.xml.ws.util;
+
+import com.sun.xml.ws.sandbox.pipe.Pipe;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * General-purpose object pool.
+ *
+ * <p>
+ * In many parts of the runtime, we need to pool instances of objects that
+ * are expensive to create (such as JAXB objects, StAX parsers, {@link Pipe} instances.)
+ *
+ * <p>
+ * This class provides a default implementation of such a pool.
+ *
+ * TODO: improve the implementation
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public abstract class Pool<T> {
+    private final ConcurrentLinkedQueue<T> queue = new ConcurrentLinkedQueue<T>();
+
+    /**
+     * Gets a new object from the pool.
+     *
+     * <p>
+     * If no object is available in the pool, this method creates a new one.
+     *
+     * @return
+     *      always non-null.
+     */
+    public final T take() {
+        T t = queue.poll();
+        if(t==null)
+            return create();
+        return t;
+    }
+
+    /**
+     * Returns an object back to the pool.
+     */
+    public final void recycle(T t) {
+        queue.offer(t);
+    }
+
+    /**
+     * Creates a new instance of object.
+     *
+     * <p>
+     * This method is used when someone wants to
+     * {@link #take() take} an object from an empty pool.
+     *
+     * <p>
+     * Also note that multiple threads may call this method
+     * concurrently.
+     */
+    protected abstract T create();
+}
