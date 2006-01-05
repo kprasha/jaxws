32429106a625bb98d8ab13becf472a7fd0333aea
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-01-05 00:20:28 +0000

    added handling of request/response context.

--- jaxws-ri/rt/src/com/sun/xml/ws/client/Stub.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/client/Stub.java
@@ -5,17 +5,21 @@ import com.sun.xml.ws.sandbox.pipe.Pipe;
 import com.sun.xml.ws.sandbox.pipe.PipeCloner;
 import com.sun.xml.ws.util.Pool;
 
+import javax.xml.ws.Binding;
+import javax.xml.ws.BindingProvider;
+
 /**
  * Base class for stubs, which accept method invocations from
  * client applications and pass the message to a {@link Pipe}
  * for processing.
  *
  * <p>
- * This class implements the management of pipe instances.
+ * This class implements the management of pipe instances,
+ * and most of the {@link BindingProvider} methods.
  *
  * @author Kohsuke Kawaguchi
  */
-public abstract class Stub {
+public abstract class Stub implements BindingProvider {
 
     /**
      * Reuse pipelines as it's expensive to create.
@@ -37,14 +41,21 @@ public abstract class Stub {
      */
     private final Pipe master;
 
+    protected final Binding binding;
+
+    private RequestContext requestContext = new RequestContext(this);
+    private ResponseContext responseContext;
 
     /**
-     *
      * @param master
      *      The created stub will send messages to this pipe.
+     * @param binding
+     *      As a {@link BindingProvider}, this object will
+     *      return this binding from {@link BindingProvider#getBinding()}.
      */
-    protected Stub(Pipe master) {
+    protected Stub(Pipe master, Binding binding) {
         this.master = master;
+        this.binding = binding;
         pipes.recycle(master);
     }
 
@@ -64,5 +75,17 @@ public abstract class Stub {
             // put it back to the pool
             pipes.recycle(pipe);
         }
+    }
+
+    public final Binding getBinding() {
+        return binding;
+    }
+
+    public final RequestContext getRequestContext() {
+        return requestContext;
+    }
+
+    public final ResponseContext getResponseContext() {
+        return responseContext;
     }
 }
