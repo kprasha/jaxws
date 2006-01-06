068d8de76d7200369986c7bdb49d053725002441
Ken Walsh <ken.walsh@sun.com>
2006-01-06 18:08:37 +0000

    Added rearch package under client.dispatch package. This is temp. Added DispatchFactory which creates typed Dispatch<T>. Abstract class DispatchImpl<T> extends new Stub If but  integration not done.

---
+++ jaxws-ri/rt/src/com/sun/xml/ws/client/dispatch/rearch/soapmsg/SOAPMessageDispatch.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2006 Sun Microsystems Corporation. All Rights Reserved.
+ */
+
+package com.sun.xml.ws.client.dispatch.rearch.soapmsg;
+
+import com.sun.xml.ws.client.dispatch.rearch.DispatchImpl;
+import com.sun.xml.ws.sandbox.message.Message;
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.*;
+import java.util.concurrent.Future;
+/**
+ * TODO: Use sandbox classes, update javadoc
+ */
+
+/**
+ * The <code>javax.xml.ws.Dispatch</code> interface provides support
+ * for the dynamic invocation of a service endpoint operation using XML
+ * constructs or JAXB objects. The <code>javax.xml.ws.Service</code>
+ * interface acts as a factory for the creation of <code>Dispatch</code>
+ * instances.
+ *
+ * @author WS Development Team
+ * @version 1.0
+ */
+
+public class SOAPMessageDispatch extends DispatchImpl<SOAPMessage> {
+    /**
+     *
+     * @param port
+     * @param aClass
+     * @param mode
+     * @param service
+     */
+    public SOAPMessageDispatch(QName port, Class<SOAPMessage> aClass, Service.Mode mode, Object service) {
+        super(port, aClass, mode, service);
+    }
+
+    /**
+     * Invoke a service operation synchronously.
+     * <p/>
+     * The client is responsible for ensuring that the <code>msg</code> object
+     * is formed according to the requirements of the protocol binding in use.
+     *
+     * @param msg An object that will form the payload of
+     *            the message used to invoke the operation. Must be an instance of
+     *            either <code>javax.xml.transform.Source</code> or a JAXB object. If
+     *            <code>msg</code> is an instance of a JAXB object then the request
+     *            context must have the <code>javax.xml.ws.binding.context</code>
+     *            property set.
+     * @return The response to the operation invocation. The object is
+     *         either an instance of <code>javax.xml.transform.Source</code>
+     *         or a JAXB object.
+     * @throws javax.xml.ws.WebServiceException
+     *          If there is any error in the configuration of
+     *          the <code>Dispatch</code> instance
+     * @throws javax.xml.ws.WebServiceException
+     *          If an error occurs when using a supplied
+     *          JAXBContext to marshall msg or unmarshall the response. The cause of
+     *          the WebServiceException is the original JAXBException.
+     */
+    public SOAPMessage invoke(SOAPMessage msg)
+        throws WebServiceException {
+
+       return null;
+    }
+
+    /**
+     *
+     *
+     * @param arg
+     * @return
+     */
+    protected Message createMessage(SOAPMessage arg) {
+        return null;
+    }
+
+    /**
+     * Invoke a service operation asynchronously.  The
+     * method returns without waiting for the response to the operation
+     * invocation, the results of the operation are obtained by polling the
+     * returned <code>Response</code>.
+     * <p/>
+     * The client is responsible for ensuring that the <code>msg</code> object
+     * when marshalled is formed according to the requirements of the protocol
+     * binding in use.
+     *
+     * @param msg An object that, when marshalled, will form the payload of
+     *            the message used to invoke the operation. Must be an instance of
+     *            either <code>javax.xml.transform.Source</code> or a JAXB object. If
+     *            <code>msg</code> is an instance of a JAXB object then the request
+     *            context must have the <code>javax.xml.ws.binding.context</code>
+     *            property set.
+     * @return The response to the operation invocation. The object
+     *         returned by <code>Response.get()</code> is
+     *         either an instance of <code>javax.xml.transform.Source</code>
+     *         or a JAXB object.
+     * @throws javax.xml.ws.WebServiceException
+     *          If there is any error in the configuration of
+     *          the <code>Dispatch</code> instance
+     * @throws javax.xml.ws.WebServiceException
+     *          If an error occurs when using a supplied
+     *          JAXBContext to marshall msg. The cause of
+     *          the WebServicException is the original JAXBException.
+     */
+    public Response<SOAPMessage> invokeAsync(SOAPMessage msg)
+        throws WebServiceException {
+
+        return null;
+    }
+
+
+    /**
+     * Invoke a service operation asynchronously. The
+     * method returns without waiting for the response to the operation
+     * invocation, the results of the operation are communicated to the client
+     * via the passed in handler.
+     * <p/>
+     * The client is responsible for ensuring that the <code>msg</code> object
+     * when marshalled is formed according to the requirements of the protocol
+     * binding in use.
+     *
+     * @param msg     An object that, when marshalled, will form the payload of
+     *                the message used to invoke the operation. Must be an instance of
+     *                either <code>javax.xml.transform.Source</code> or a JAXB object. If
+     *                <code>msg</code> is an instance of a JAXB object then the request
+     *                context must have the <code>javax.xml.ws.binding.context</code>
+     *                property set.
+     * @param handler The handler object that will receive the
+     *                response to the operation invocation. The object
+     *                returned by <code>Response.get()</code> is
+     *                either an instance of
+     *                <code>javax.xml.transform.Source</code> or a JAXB object.
+     * @return A <code>Future</code> object that may be used to check the status
+     *         of the operation invocation. This object must not be used to try to
+     *         obtain the results of the operation - the object returned from
+     *         <code>Future<?>.get()</code> is implementation dependent
+     *         and any use of it will result in non-portable behaviour.
+     * @throws javax.xml.ws.WebServiceException
+     *          If there is any error in the configuration of
+     *          the <code>Dispatch</code> instance
+     * @throws javax.xml.ws.WebServiceException
+     *          If an error occurs when using a supplied
+     *          JAXBContext to marshall msg. The cause of
+     *          the WebServiceException is the original JAXBException.
+     */
+    public Future<?> invokeAsync(SOAPMessage msg, AsyncHandler<SOAPMessage> handler) {
+
+        return null;
+    }
+
+    /**
+     * Invokes a service operation using the one-way
+     * interaction mode. The operation invocation is logically non-blocking,
+     * subject to the capabilities of the underlying protocol, no results
+     * are returned. When
+     * the protocol in use is SOAP/HTTP, this method must block until
+     * an HTTP response code has been received or an error occurs.
+     * <p/>
+     * The client is responsible for ensuring that the <code>msg</code> object
+     * when marshalled is formed according to the requirements of the protocol
+     * binding in use.
+     *
+     * @param msg An object that, when marshalled, will form the payload of
+     *            the message used to invoke the operation. Must be an instance of
+     *            either <code>javax.xml.transform.Source</code> or a JAXB object. If
+     *            <code>msg</code> is an instance of a JAXB object then the request
+     *            context must have the <code>javax.xml.ws.binding.context</code>
+     *            property set.
+     * @throws javax.xml.ws.WebServiceException
+     *          If there is any error in the configuration of
+     *          the <code>Dispatch</code> instance or if an error occurs during the
+     *          invocation.
+     * @throws javax.xml.ws.WebServiceException
+     *          If an error occurs when using a supplied
+     *          JAXBContext to marshall msg. The cause of
+     *          the WebServiceException is the original JAXBException.
+     */
+
+    public void invokeOneWay(SOAPMessage msg) {
+
+    }
+}
---
+++ jaxws-ri/rt/src/com/sun/xml/ws/client/dispatch/rearch/source/SourceDispatch.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2006 Sun Microsystems Corporation. All Rights Reserved.
+ */
+
+package com.sun.xml.ws.client.dispatch.rearch.source;
+
+import com.sun.xml.ws.client.dispatch.rearch.DispatchImpl;
+import com.sun.xml.ws.sandbox.message.Message;
+
+import javax.xml.namespace.QName;
+import javax.xml.transform.Source;
+import javax.xml.ws.*;
+import java.util.Map;
+import java.util.concurrent.Future;
+/**
+ * TODO: Use sandbox classes, update javadoc
+ */
+
+/**
+ * The <code>javax.xml.ws.Dispatch</code> interface provides support
+ * for the dynamic invocation of a service endpoint operation using XML
+ * constructs or JAXB objects. The <code>javax.xml.ws.Service</code>
+ * interface acts as a factory for the creation of <code>Dispatch</code>
+ * instances.
+ *
+ * @author WS Development Team
+ * @version 1.0
+ */
+
+public class SourceDispatch extends DispatchImpl<Source> {
+
+    public SourceDispatch(QName port, Class<Source> aClass, Service.Mode mode, Object service) {
+        super(port, aClass, mode, service);
+    }
+
+
+    /**
+     * Invoke a service operation synchronously.
+     * <p/>
+     * The client is responsible for ensuring that the <code>msg</code> object
+     * is formed according to the requirements of the protocol binding in use.
+     *
+     * @param msg An object that will form the payload of
+     *            the message used to invoke the operation. Must be an instance of
+     *            either <code>javax.xml.transform.Source</code> or a JAXB object. If
+     *            <code>msg</code> is an instance of a JAXB object then the request
+     *            context must have the <code>javax.xml.ws.binding.context</code>
+     *            property set.
+     * @return The response to the operation invocation. The object is
+     *         either an instance of <code>javax.xml.transform.Source</code>
+     *         or a JAXB object.
+     * @throws javax.xml.ws.WebServiceException
+     *          If there is any error in the configuration of
+     *          the <code>Dispatch</code> instance
+     * @throws javax.xml.ws.WebServiceException
+     *          If an error occurs when using a supplied
+     *          JAXBContext to marshall msg or unmarshall the response. The cause of
+     *          the WebServiceException is the original JAXBException.
+     */
+    public Source invoke(Source msg)
+        throws WebServiceException {
+        return null;
+    }
+
+    /**
+     * Invoke a service operation asynchronously.  The
+     * method returns without waiting for the response to the operation
+     * invocation, the results of the operation are obtained by polling the
+     * returned <code>Response</code>.
+     * <p/>
+     * The client is responsible for ensuring that the <code>msg</code> object
+     * when marshalled is formed according to the requirements of the protocol
+     * binding in use.
+     *
+     * @param msg An object that, when marshalled, will form the payload of
+     *            the message used to invoke the operation. Must be an instance of
+     *            either <code>javax.xml.transform.Source</code> or a JAXB object. If
+     *            <code>msg</code> is an instance of a JAXB object then the request
+     *            context must have the <code>javax.xml.ws.binding.context</code>
+     *            property set.
+     * @return The response to the operation invocation. The object
+     *         returned by <code>Response.get()</code> is
+     *         either an instance of <code>javax.xml.transform.Source</code>
+     *         or a JAXB object.
+     * @throws javax.xml.ws.WebServiceException
+     *          If there is any error in the configuration of
+     *          the <code>Dispatch</code> instance
+     * @throws javax.xml.ws.WebServiceException
+     *          If an error occurs when using a supplied
+     *          JAXBContext to marshall msg. The cause of
+     *          the WebServicException is the original JAXBException.
+     */
+    public Response<Source> invokeAsync(Source msg)
+        throws WebServiceException {
+
+        return null;
+    }
+
+
+    /**
+     * Invoke a service operation asynchronously. The
+     * method returns without waiting for the response to the operation
+     * invocation, the results of the operation are communicated to the client
+     * via the passed in handler.
+     * <p/>
+     * The client is responsible for ensuring that the <code>msg</code> object
+     * when marshalled is formed according to the requirements of the protocol
+     * binding in use.
+     *
+     * @param msg     An object that, when marshalled, will form the payload of
+     *                the message used to invoke the operation. Must be an instance of
+     *                either <code>javax.xml.transform.Source</code> or a JAXB object. If
+     *                <code>msg</code> is an instance of a JAXB object then the request
+     *                context must have the <code>javax.xml.ws.binding.context</code>
+     *                property set.
+     * @param handler The handler object that will receive the
+     *                response to the operation invocation. The object
+     *                returned by <code>Response.get()</code> is
+     *                either an instance of
+     *                <code>javax.xml.transform.Source</code> or a JAXB object.
+     * @return A <code>Future</code> object that may be used to check the status
+     *         of the operation invocation. This object must not be used to try to
+     *         obtain the results of the operation - the object returned from
+     *         <code>Future<?>.get()</code> is implementation dependent
+     *         and any use of it will result in non-portable behaviour.
+     * @throws javax.xml.ws.WebServiceException
+     *          If there is any error in the configuration of
+     *          the <code>Dispatch</code> instance
+     * @throws javax.xml.ws.WebServiceException
+     *          If an error occurs when using a supplied
+     *          JAXBContext to marshall msg. The cause of
+     *          the WebServiceException is the original JAXBException.
+     */
+    public Future<?> invokeAsync(Source msg, AsyncHandler<Source> handler) {
+
+        return null;
+    }
+
+    /**
+     * Invokes a service operation using the one-way
+     * interaction mode. The operation invocation is logically non-blocking,
+     * subject to the capabilities of the underlying protocol, no results
+     * are returned. When
+     * the protocol in use is SOAP/HTTP, this method must block until
+     * an HTTP response code has been received or an error occurs.
+     * <p/>
+     * The client is responsible for ensuring that the <code>msg</code> object
+     * when marshalled is formed according to the requirements of the protocol
+     * binding in use.
+     *
+     * @param msg An object that, when marshalled, will form the payload of
+     *            the message used to invoke the operation. Must be an instance of
+     *            either <code>javax.xml.transform.Source</code> or a JAXB object. If
+     *            <code>msg</code> is an instance of a JAXB object then the request
+     *            context must have the <code>javax.xml.ws.binding.context</code>
+     *            property set.
+     * @throws javax.xml.ws.WebServiceException
+     *          If there is any error in the configuration of
+     *          the <code>Dispatch</code> instance or if an error occurs during the
+     *          invocation.
+     * @throws javax.xml.ws.WebServiceException
+     *          If an error occurs when using a supplied
+     *          JAXBContext to marshall msg. The cause of
+     *          the WebServiceException is the original JAXBException.
+     */
+
+    public void invokeOneWay(Source msg) {
+
+
+    }
+
+
+    /**
+     * Get the jaxwsContext that resulted from processing a response message.
+     * <p/>
+     * The returned context is for the most recently completed synchronous
+     * operation. Subsequent synchronous operation invocations overwrite the
+     * response context. Asynchronous operations return their response context
+     * via the Response interface.
+     *
+     * @return The jaxwsContext that is used in processing request messages.
+     */
+
+    protected Message createMessage(Source msg) {
+        return null;
+    }
+}
