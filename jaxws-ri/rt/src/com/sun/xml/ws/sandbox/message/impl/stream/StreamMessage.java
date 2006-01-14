4434e24285cb314dd1f2cb8c1a45bad689fd6ff9
Vivek Pandey <vivek.pandey@sun.com>
2006-01-14 02:33:12 +0000

    * Added flag to identify if StreamMessage is created using payload or Envelope. Probably we may need different classes for payload and Envelope. Lets keep this way for the time being
    * Modified constructor - added SOAPVersion
    StreamMessage(HeaderList, XMLStreamReader, SOAPVersion)
    
    * Implemented:
     - writePayloadTo()
     - writeTo()

--- jaxws-ri/rt/src/com/sun/xml/ws/sandbox/message/impl/stream/StreamMessage.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/sandbox/message/impl/stream/StreamMessage.java
@@ -19,24 +19,37 @@
  */
 package com.sun.xml.ws.sandbox.message.impl.stream;
 
+import com.sun.xml.ws.api.message.Header;
 import com.sun.xml.ws.api.message.HeaderList;
 import com.sun.xml.ws.api.message.Message;
 import com.sun.xml.ws.api.message.MessageProperties;
+import com.sun.xml.ws.encoding.soap.SOAPVersion;
 import com.sun.xml.ws.sandbox.message.impl.AbstractMessageImpl;
 import com.sun.xml.ws.util.xml.StAXSource;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.soap.SOAPMessage;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.stream.XMLStreamWriter;
-import javax.xml.transform.Source;
-
 import org.xml.sax.ContentHandler;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.transform.Source;
+
 public class StreamMessage extends AbstractMessageImpl {
     private final MessageProperties props = new MessageProperties();
+
+    /**
+     * flag that tells whether StreamMessage is created with the
+     * XMLStreamReader that points to the payload or soapenv:Envelop
+     *
+     * Probably we may need different classes for payload and Envelope.
+     * Lets keep this way for the timebeing.
+     */
+    private boolean isPayload;
 
     /**
      * The reader will be positioned at
@@ -51,6 +64,8 @@ public class StreamMessage extends AbstractMessageImpl {
 
     private final String payloadNamespaceURI;
 
+    private final SOAPVersion soapVersion;
+
     /**
      * Creates a {@link StreamMessage} from a {@link XMLStreamReader}
      * that points at the start element of the payload, and headers.
@@ -64,11 +79,13 @@ public class StreamMessage extends AbstractMessageImpl {
      * @param reader
      *      must not be null.
      */
-    public StreamMessage(HeaderList headers, XMLStreamReader reader) {
+    public StreamMessage(HeaderList headers, XMLStreamReader reader, SOAPVersion soapVersion) {
         this.headers = headers;
         this.reader = reader;
         this.payloadLocalName = reader.getLocalName();
         this.payloadNamespaceURI = reader.getNamespaceURI();
+        this.soapVersion = soapVersion;
+        isPayload = true;
     }
 
     /**
@@ -128,22 +145,178 @@ public class StreamMessage extends AbstractMessageImpl {
         return this.reader;
     }
 
+    public void writePayloadTo(XMLStreamWriter writer)throws XMLStreamException {
+        int state;
+        boolean firsttime = true;
+        do {
+            if(firsttime){
+                state = reader.getEventType();
+                firsttime = false;
+            }else{
+                state = reader.next();
+            }
+            switch (state) {
+                case XMLStreamConstants.START_ELEMENT:
+                    /*
+                     * TODO: Is this necessary, shouldn't zephyr return "" instead of
+                     * null for getNamespaceURI() and getPrefix()?
+                     */
+                    String uri = reader.getNamespaceURI();
+                    String prefix = reader.getPrefix();
+                    String localName = reader.getLocalName();
 
+                    if (prefix == null) {
+                        if (uri == null) {
+                            writer.writeStartElement(localName);
+                        } else {
+                            writer.writeStartElement(uri, localName);
+                        }
+                    } else {
+                        assert uri != null;
 
-    public void writePayloadTo(XMLStreamWriter sw) {
-        throw new UnsupportedOperationException();
+                        if(prefix.length() > 0){
+                            /**
+                             * Before we write the
+                             */
+                            String writerURI = null;
+                            if (writer.getNamespaceContext() != null)
+                                writerURI = writer.getNamespaceContext().getNamespaceURI(prefix);
+                            String writerPrefix = writer.getPrefix(uri);
+                            if(declarePrefix(prefix, uri, writerPrefix, writerURI)){
+                                writer.writeStartElement(prefix, localName, uri);
+                                writer.setPrefix(prefix, uri != null ? uri : "");
+                                writer.writeNamespace(prefix, uri);
+                            }else{
+                                writer.writeStartElement(prefix, localName, uri);
+                            }
+                        }else{
+                            writer.writeStartElement(prefix, localName, uri);
+                        }
+                    }
+
+                    int n = reader.getNamespaceCount();
+                    // Write namespace declarations
+                    for (int i = 0; i < n; i++) {
+                        String nsPrefix = reader.getNamespacePrefix(i);
+                        if (nsPrefix == null) nsPrefix = "";
+                        // StAX returns null for default ns
+                        String writerURI = null;
+                        if (writer.getNamespaceContext() != null)
+                            writerURI = writer.getNamespaceContext().getNamespaceURI(nsPrefix);
+
+                        // Zephyr: Why is this returning null?
+                        // Compare nsPrefix with prefix because of [1] (above)
+                        String readerURI = reader.getNamespaceURI(i);
+
+                        /**
+                         * write the namespace in 3 conditions
+                         *  - when the namespace URI is not bound to the prefix in writer(writerURI == 0)
+                         *  - when the readerPrefix and writerPrefix are ""
+                         *  - when readerPrefix and writerPrefix are not equal and the URI bound to them
+                         *    are different
+                         */
+                        if (writerURI == null || ((nsPrefix.length() == 0) || (prefix.length() == 0)) ||
+                                (!nsPrefix.equals(prefix) && !writerURI.equals(readerURI))) {
+                            writer.setPrefix(nsPrefix, readerURI != null ? readerURI : "");
+                            writer.writeNamespace(nsPrefix, readerURI != null ? readerURI : "");
+                        }
+                    }
+
+                    // Write attributes
+                    n = reader.getAttributeCount();
+                    for (int i = 0; i < n; i++) {
+                        String attrPrefix = reader.getAttributePrefix(i);
+                        String attrURI = reader.getAttributeNamespace(i);
+
+                        writer.writeAttribute(attrPrefix != null ? attrPrefix : "",
+                            attrURI != null ? attrURI : "",
+                            reader.getAttributeLocalName(i),
+                            reader.getAttributeValue(i));
+                        // if the attribute prefix is undeclared in current writer scope then declare it
+                        setUndeclaredPrefix(attrPrefix, attrURI, writer);
+                    }
+                    break;
+                case XMLStreamConstants.END_ELEMENT:
+                    writer.writeEndElement();
+                    break;
+                case XMLStreamConstants.CHARACTERS:
+                    writer.writeCharacters(reader.getText());
+            }
+        } while (state != XMLStreamConstants.END_DOCUMENT);
+        reader.close();
+
     }
 
-    public void writeTo(XMLStreamWriter sw) {
-        throw new UnsupportedOperationException();
+    /**
+     * sets undeclared prefixes on the writer
+     * @param prefix
+     * @param writer
+     * @throws XMLStreamException
+     */
+    private void setUndeclaredPrefix(String prefix, String readerURI, XMLStreamWriter writer) throws XMLStreamException {
+        String writerURI = null;
+        if (writer.getNamespaceContext() != null)
+            writerURI = writer.getNamespaceContext().getNamespaceURI(prefix);
+
+        if (writerURI == null) {
+            writer.setPrefix(prefix, readerURI != null ? readerURI : "");
+            writer.writeNamespace(prefix, readerURI != null ? readerURI : "");
+        }
+    }
+
+    /**
+     * check if we need to declare
+     * @param rPrefix
+     * @param rUri
+     * @param wPrefix
+     * @param wUri
+     */
+    private boolean declarePrefix(String rPrefix, String rUri, String wPrefix, String wUri){
+        if (wUri == null ||((wPrefix != null) && !rPrefix.equals(wPrefix))||
+                (rUri != null && !wUri.equals(rUri)))
+            return true;
+        return false;
+    }
+
+    public void writeTo(XMLStreamWriter sw) throws XMLStreamException{
+        if(isPayload){
+            writeEnvelope(sw);
+        }else{
+            //TODO
+        }
+    }
+
+    /**
+     * This method should be called when the StreamMessage is created with a payload
+     * @param writer
+     */
+    private void writeEnvelope(XMLStreamWriter writer) throws XMLStreamException {
+        writer.writeStartElement("soapenv", "Envelope", soapVersion.nsUri);
+        writer.writeNamespace("soapenv", soapVersion.nsUri);
+        //TODO: collect all the namespaces from the payload and add to Envelope element
+
+        //write headers
+        HeaderList hl = getHeaders();
+        if(hl.size() > 0){
+            writer.writeStartElement("soapenv", "Header", soapVersion.nsUri);
+            for(Header h:hl){
+                h.writeTo(writer);
+            }
+            writer.writeEndElement();
+        }
+        writer.writeStartElement("soapenv", "Body", soapVersion.nsUri);
+        writePayloadTo(writer);
+        writer.writeEndElement();
+        writer.writeEndElement();
     }
 
     public void writeTo(ContentHandler contentHandler, ErrorHandler errorHandler) throws SAXException {
-        // TODO
         throw new UnsupportedOperationException();
     }
 
     public Message copy() {
         throw new UnsupportedOperationException();
     }
+
+
 }
