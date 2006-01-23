5676b293b353979d024240648cdd236ee0e13bd4
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-01-23 19:22:22 +0000

    added an utility class to dump the messages. On a hindsight, perhaps this should have gone to the test workspaec?

---
+++ jaxws-ri/rt/src/com/sun/xml/ws/util/pipe/DumpPipe.java
@@ -0,0 +1,60 @@
+package com.sun.xml.ws.util.pipe;
+
+import com.sun.xml.ws.api.pipe.Pipe;
+import com.sun.xml.ws.api.pipe.PipeCloner;
+import com.sun.xml.ws.api.message.Message;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import java.io.PrintStream;
+
+/**
+ * {@link Pipe} that dumps messages that pass through.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class DumpPipe implements Pipe {
+    private final Pipe next;
+
+    private final PrintStream out;
+
+    private final XMLOutputFactory staxOut;
+
+    /**
+     * @param out
+     *      The output to send dumps to.
+     * @param next
+     *      The next {@link Pipe} in the pipeline.
+     */
+    public DumpPipe(PrintStream out, Pipe next) {
+        this.out = out;
+        this.next = next;
+        this.staxOut = XMLOutputFactory.newInstance();
+    }
+
+    public Message process(Message msg) {
+        dump("request",msg);
+        Message reply = next.process(msg);
+        dump("response",reply);
+        return reply;
+    }
+
+    private void dump(String header, Message msg) {
+        out.println("====["+header+"]====");
+        try {
+            msg.copy().writeTo(staxOut.createXMLStreamWriter(out));
+        } catch (XMLStreamException e) {
+            e.printStackTrace(out);
+        }
+        out.println("============");
+    }
+
+
+    public Pipe copy(PipeCloner cloner) {
+        return new DumpPipe(out,cloner.copy(next));
+    }
+
+    public void preDestroy() {
+        // noop
+    }
+}
