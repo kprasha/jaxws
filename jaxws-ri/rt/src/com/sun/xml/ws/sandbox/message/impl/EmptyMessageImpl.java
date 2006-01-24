cb7a0e09c831fad89e4ab8930658e6f4b8419fb7
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-01-24 23:19:12 +0000

    eventually we should have an EmptyMessage implementation like this. for now it's just a place holder.

---
+++ jaxws-ri/rt/src/com/sun/xml/ws/sandbox/message/impl/EmptyMessageImpl.java
@@ -0,0 +1,105 @@
+package com.sun.xml.ws.sandbox.message.impl;
+
+import com.sun.xml.ws.api.message.Message;
+import com.sun.xml.ws.api.message.HeaderList;
+import com.sun.xml.ws.api.message.MessageProperties;
+
+import javax.xml.transform.Source;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.xml.sax.ContentHandler;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+
+/**
+ * {@link Message} that has no body.
+ *
+ * TODO: this is a work in progress.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class EmptyMessageImpl extends AbstractMessageImpl {
+
+    /**
+     * If a message has no payload, it's more likely to have
+     * some header, so we create it eagerly here.
+     */
+    private final HeaderList headers;
+
+    private final MessageProperties properties;
+
+    public EmptyMessageImpl() {
+        headers = new HeaderList();
+        properties = new MessageProperties();
+    }
+
+    /**
+     * Copy constructor.
+     */
+    private EmptyMessageImpl(EmptyMessageImpl that) {
+        this.headers = new HeaderList(that.headers);
+        this.properties = that.properties;
+    }
+
+    public boolean hasHeaders() {
+        return !headers.isEmpty();
+    }
+
+    public HeaderList getHeaders() {
+        return headers;
+    }
+
+    public MessageProperties getProperties() {
+        return properties;
+    }
+
+    public String getPayloadLocalPart() {
+        return null;
+    }
+
+    public String getPayloadNamespaceURI() {
+        return null;
+    }
+
+    public boolean hasPayload() {
+        return false;
+    }
+
+    public Source readPayloadAsSource() {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    public SOAPMessage readAsSOAPMessage() throws SOAPException {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    public XMLStreamReader readPayload() throws XMLStreamException {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    public void writePayloadTo(XMLStreamWriter sw) throws XMLStreamException {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    public void writeTo(XMLStreamWriter sw) throws XMLStreamException {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    public void writeTo(ContentHandler contentHandler, ErrorHandler errorHandler) throws SAXException {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    public Message copy() {
+        return new EmptyMessageImpl(this);
+    }
+}
