ced139acdd2afe01a52ff02b27a63d9f73ebfb87
Vivek Pandey <vivek.pandey@sun.com>
2006-01-25 02:22:50 +0000

    Partial implementation of protocol source Message.

---
+++ jaxws-ri/rt/src/com/sun/xml/ws/sandbox/message/impl/source/ProtocolSourceMessage.java
@@ -0,0 +1,103 @@
+package com.sun.xml.ws.sandbox.message.impl.source;
+
+import com.sun.xml.ws.api.message.AttachmentSet;
+import com.sun.xml.ws.api.message.HeaderList;
+import com.sun.xml.ws.api.message.Message;
+import com.sun.xml.ws.api.message.MessageProperties;
+import com.sun.xml.ws.sandbox.message.impl.AbstractMessageImpl;
+import com.sun.xml.ws.sandbox.message.impl.stream.StreamMessage;
+import com.sun.xml.ws.streaming.XMLStreamReaderFactory;
+import com.sun.xml.ws.streaming.XMLStreamReaderUtil;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+
+/**
+ * Partial implementation of {@link Message} backed by {@link Source} where the Source represents the complete message
+ * such as a SOAP envelope.
+ *
+ * @author Vivek Pandey
+ */
+public class ProtocolSourceMessage extends AbstractMessageImpl {
+    private HeaderList headers;
+    private AttachmentSet attSet;
+    private Source src;
+    private MessageProperties properties;
+    private StreamMessage sm;
+
+    public ProtocolSourceMessage(Source source) {
+        this.src = source;
+        SourceUtils sourceUtils = new SourceUtils(src);
+        if (sourceUtils.isStreamSource()) {
+            StreamSource streamSource = (StreamSource) src;
+            XMLStreamReader reader = XMLStreamReaderFactory.createXMLStreamReader(streamSource.getInputStream(), true);
+            //move the cursor to the payload element
+            XMLStreamReaderUtil.next(reader);
+            sm = new StreamMessage(reader);
+        }
+    }
+
+    public boolean hasHeaders() {
+        return (headers != null) && headers.size() > 0;
+    }
+
+    public HeaderList getHeaders() {
+        if (headers == null)
+            headers = new HeaderList();
+        return headers;
+    }
+
+    public MessageProperties getProperties() {
+        if (properties == null)
+            return properties = new MessageProperties();
+        return properties;
+    }
+
+    public String getPayloadLocalPart() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getPayloadNamespaceURI() {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean hasPayload() {
+        throw new UnsupportedOperationException();
+    }
+
+    public Source readPayloadAsSource() {
+        throw new UnsupportedOperationException();
+    }
+
+    public SOAPMessage readAsSOAPMessage() throws SOAPException {
+        throw new UnsupportedOperationException();
+    }
+
+    public XMLStreamReader readPayload() throws XMLStreamException {
+        throw new UnsupportedOperationException();
+    }
+
+    public void writePayloadTo(XMLStreamWriter sw) throws XMLStreamException {
+        throw new UnsupportedOperationException();
+    }
+
+    public void writeTo(XMLStreamWriter sw) throws XMLStreamException {
+        throw new UnsupportedOperationException();
+    }
+
+    public void writeTo(ContentHandler contentHandler, ErrorHandler errorHandler) throws SAXException {
+        throw new UnsupportedOperationException();
+    }
+
+    public Message copy() {
+        throw new UnsupportedOperationException();
+    }
+}
