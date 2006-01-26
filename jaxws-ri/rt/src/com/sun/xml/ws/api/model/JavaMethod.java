91eb94ec4ac38925790415882c6348d75b42d4b3
Vivek Pandey <vivek.pandey@sun.com>
2006-01-26 03:00:53 +0000

    Reverting back - it needs more work and causes compilation failures.

--- jaxws-ri/rt/src/com/sun/xml/ws/api/model/JavaMethod.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/api/model/JavaMethod.java
@@ -46,7 +46,7 @@ public interface JavaMethod {
      *
      * @return returns unmodifiable list of request parameters
      */
-    List<? extends Parameter> getRequestParameters();
+    List<Parameter> getRequestParameters();
 
     /**
      * Response parameters go in the response message on-the-wire and can be of
@@ -54,7 +54,7 @@ public interface JavaMethod {
      * {@link com.sun.xml.ws.api.model.Mode#INOUT}
      * @return returns unmodifiable list of response parameters
      */
-    List<? extends Parameter> getResponseParameters();
+    List<Parameter> getResponseParameters();
 
     /**
      * @return Returns number of java method parameters - that will be all the
