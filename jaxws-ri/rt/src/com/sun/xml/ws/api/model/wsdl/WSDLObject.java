cf8ba0fd994af45d07d6a247aeffd44c814665ae
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-02-03 00:39:43 +0000

    formatting changes.

--- jaxws-ri/rt/src/com/sun/xml/ws/api/model/wsdl/WSDLObject.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/api/model/wsdl/WSDLObject.java
@@ -1,8 +1,5 @@
 package com.sun.xml.ws.api.model.wsdl;
 
-import com.sun.xml.ws.wsdl.parser.WSDLConstants;
-
-
 /**
  * Marker interface serves as base interface for the wsdl model
  * @author Vivek Pandey
