5513e55694e33e4348fbd948c7e3c55505ad7a94
Vivek Pandey <vivek.pandey@sun.com>
2006-02-07 00:32:14 +0000

    Java code generator extension interface.

---
+++ jaxws-ri/tools/wscompile/src/com/sun/tools/ws/api/JavaGeneratorExtension.java
@@ -0,0 +1,19 @@
+package com.sun.tools.ws.api;
+
+import com.sun.codemodel.JMethod;
+import com.sun.tools.ws.api.wsdl.TExtensible;
+
+/**
+ * Provides Java SEI Code generation Extensiblity mechanism.
+ *
+ * @author Vivek Pandey
+ */
+public interface JavaGeneratorExtension {
+    /**
+     * This method should be used to write annotations on {@link JMethod}
+     *
+     * @param wsdlOperation non-null wsdl extensiblity element -  wsdl:operation.
+     * @param jMethod non-null {@link JMethod}
+     */
+    void writeOperationAnnotations(TExtensible wsdlOperation, JMethod jMethod);
+}
