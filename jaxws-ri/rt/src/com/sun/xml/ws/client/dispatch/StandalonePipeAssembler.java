1c6593ea3f9c0d57a95a398c6c494ed5ed4231b8
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2006-02-11 02:36:29 +0000

    removing HandlerPipe temporarily from PipelineAssembler

--- jaxws-ri/rt/src/com/sun/xml/ws/client/dispatch/StandalonePipeAssembler.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/client/dispatch/StandalonePipeAssembler.java
@@ -9,16 +9,18 @@ import com.sun.xml.ws.api.WSService;
 import com.sun.xml.ws.api.model.wsdl.WSDLPort;
 import com.sun.xml.ws.api.pipe.Pipe;
 import com.sun.xml.ws.api.pipe.PipelineAssembler;
-import com.sun.xml.ws.sandbox.handler.ClientHandlerPipe;
+//import com.sun.xml.ws.sandbox.handler.ClientHandlerPipe;
 import com.sun.xml.ws.transport.http.client.HttpTransportPipe;
 
 public class StandalonePipeAssembler implements PipelineAssembler {
     public Pipe createClient(WSDLPort wsdlModel, WSService service, WSBinding binding) {
         Pipe head = createTransport(wsdlModel,service,binding);
+        /*
         if(!binding.getHandlerChain().isEmpty()) {
             Pipe handlerPipe = new ClientHandlerPipe(binding, head);
             head = handlerPipe;
         }
+         */
         return head;
     }
 
