b290652fe942490657551530ca30e723c61d50cd
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2006-04-12 02:09:26 +0000

    LogicalHandlerPipe was not set while creating Server Pipeline.

--- jaxws-ri/rt/src/com/sun/xml/ws/client/dispatch/StandalonePipeAssembler.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/client/dispatch/StandalonePipeAssembler.java
@@ -3,12 +3,12 @@
  * of the Common Development and Distribution License
  * (the License).  You may not use this file except in
  * compliance with the License.
- * 
+ *
  * You can obtain a copy of the license at
  * https://glassfish.dev.java.net/public/CDDLv1.0.html.
  * See the License for the specific language governing
  * permissions and limitations under the License.
- * 
+ *
  * When distributing Covered Code, include this CDDL
  * Header Notice in each file and include the License file
  * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
@@ -16,7 +16,7 @@
  * with the fields enclosed by brackets [] replaced by
  * you own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
  */
 
@@ -95,7 +95,8 @@ public class StandalonePipeAssembler implements PipelineAssembler {
             boolean isClient = false;
             HandlerPipe logicalHandlerPipe =
                     new LogicalHandlerPipe(binding, wsdlModel, terminal, isClient);
-
+            terminal = logicalHandlerPipe;
+            
             //Someother pipes like JAX-WSA Pipe can come in between LogicalHandlerPipe and
             //SOAPHandlerPipe here.
 
