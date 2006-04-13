224efa60b26f72b31a10bf77a9eb5ca471a30cdb
Ken Walsh <ken.walsh@sun.com>
2006-04-13 19:05:37 +0000

    Implementation for DispatchDataSource now will return DataSource in toReturnValue() even if the Message type is SourceMessage.

--- jaxws-ri/rt/src/com/sun/xml/ws/client/dispatch/DataSourceDispatch.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/client/dispatch/DataSourceDispatch.java
@@ -29,10 +29,18 @@ import com.sun.xml.ws.binding.BindingImpl;
 import com.sun.xml.ws.client.WSServiceDelegate;
 import com.sun.xml.ws.encoding.xml.XMLMessage;
 import com.sun.xml.ws.encoding.xml.XMLMessage.HasDataSource;
+import com.sun.xml.ws.message.source.PayloadSourceMessage;
 
 import javax.activation.DataSource;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
+import javax.xml.ws.WebServiceException;
+import javax.xml.transform.Source;
+import javax.xml.transform.sax.SAXSource;
+
+import org.xml.sax.InputSource;
+
+import java.io.InputStream;
 
 /**
  *
@@ -46,49 +54,28 @@ public class DataSourceDispatch extends DispatchImpl<DataSource> {
     }
 
     Packet createPacket(DataSource arg) {
-        // TODO
-         //Message message = null;
-         /*switch (mode) {
-            case PAYLOAD:
-                //message = ??
-                break;
-            case MESSAGE:
-                //Todo: temporary
 
-                //todo:temp
-                //message = ?;
-                //todo: uncomment above when correct Message for DS is implemented
-                break;
+         switch (mode) {
+            case PAYLOAD:
+                throw new IllegalArgumentException("DataSource use is not allowed in Service.Mode.PAYLOAD\n");
+            case MESSAGE:
+                return new Packet(XMLMessage.create(arg));
             default:
                 throw new WebServiceException("Unrecognized message mode");
         }
-
-        return new Packet(message);
-        */
-        return new Packet(XMLMessage.create(arg));
     }
 
     DataSource toReturnValue(Packet response) {
-        // TODO
-        // TODO
-        //Message msg = response.getMessage();
-        /*switch (mode){
-            case PAYLOAD:
-                //return ?;
-            case MESSAGE:
-                //return
-                //return ?;
-            default:
-                throw new WebServiceException("Unrecognized dispatch mode");
-        }
-        */
+
         Message message = response.getMessage();
+
         if (message instanceof HasDataSource) {
             HasDataSource hasDS = (HasDataSource)message;
             // TODO Need to call hasUnconsumedDataSource()
             return hasDS.getDataSource();
+        } else if (message instanceof PayloadSourceMessage) {
+            return XMLMessage.getDataSource(message);
         }
-        // TODO need to convert message to DataSource
-        throw new UnsupportedOperationException();
+        return null;
     }
 }
