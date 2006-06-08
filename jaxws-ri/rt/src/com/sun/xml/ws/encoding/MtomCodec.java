124d1405617157461bbcd39dd74e98a69428f6e2
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-06-08 18:21:55 +0000

    another simplification for MtomCodec.

--- jaxws-ri/rt/src/com/sun/xml/ws/encoding/MtomCodec.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/encoding/MtomCodec.java
@@ -33,6 +33,7 @@ import com.sun.xml.ws.message.stream.StreamAttachment;
 import com.sun.xml.ws.streaming.XMLStreamReaderFactory;
 import com.sun.xml.ws.streaming.XMLStreamWriterFactory;
 import com.sun.xml.ws.util.xml.XMLStreamReaderFilter;
+import com.sun.xml.ws.util.xml.XMLStreamWriterFilter;
 import org.jvnet.staxex.Base64Data;
 import org.jvnet.staxex.NamespaceContextEx;
 import org.jvnet.staxex.XMLStreamReaderEx;
@@ -40,8 +41,6 @@ import org.jvnet.staxex.XMLStreamWriterEx;
 
 import javax.activation.DataHandler;
 import javax.xml.namespace.NamespaceContext;
-import javax.xml.namespace.QName;
-import javax.xml.stream.Location;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
@@ -73,9 +72,7 @@ public class MtomCodec extends MimeCodec {
     // encoding related parameters
     private String boundary;
     private final String soapXopContentType;
-    private final XMLStreamWriterEx xmlStreamWriterEx = new MtomStreamWriter();
     private String messageContentType;
-    private XMLStreamWriter writer;
     private UTF8OutputStreamWriter osWriter;
 
     //This is the mtom attachment stream, we should write it just after the root part for decoder
@@ -117,15 +114,12 @@ public class MtomCodec extends MimeCodec {
         return null;
     }
 
-    private OutputStream writeMtomBinary(String contentType){
-        throw new UnsupportedOperationException();
-    }
-
     public ContentType encode(Packet packet, OutputStream out) throws IOException {
         //get the current boundary thaat will be reaturned from this method
         mtomAttachmentStream.clear();
         ContentType contentType = getContentType(packet);
-        writer = XMLStreamWriterFactory.createXMLStreamWriter(out);
+        MtomStreamWriter writer = new MtomStreamWriter(XMLStreamWriterFactory.createXMLStreamWriter(out));
+
         osWriter = new UTF8OutputStreamWriter(out);
         if(packet.getMessage() != null){
             try {
@@ -133,7 +127,7 @@ public class MtomCodec extends MimeCodec {
                 OutputUtil.writeln("Content-Type: "+ soapXopContentType,  out);
                 OutputUtil.writeln("Content-Transfer-Encoding: binary", out);
                 OutputUtil.writeln(out);
-                packet.getMessage().writeTo(getXmlStreamWriterEx());
+                packet.getMessage().writeTo(writer);
                 OutputUtil.writeln(out);
 
                 int numOfAttachments = 0;
@@ -190,30 +184,11 @@ public class MtomCodec extends MimeCodec {
         }
     }
 
-    private void writeMtomBinary(byte[] data, int start, int len, String contentType){
-        try {
-            ByteArrayBuffer bos = new ByteArrayBuffer(data, start, len, contentType);
-            mtomAttachmentStream.add(bos);
-
-            //write out the xop reference
-            writer.writeCharacters("");
-            osWriter.write(XOP_PREF +bos.contentId+XOP_SUFF);
-        } catch (IOException e) {
-            throw new WebServiceException(e);
-        } catch (XMLStreamException e) {
-            throw new WebServiceException(e);
-        }
-    }
-
     private void writeMimeHeaders(String contentType, String contentId, OutputStream out) throws IOException {
         OutputUtil.writeln("Content-Type: " + contentType, out);
         OutputUtil.writeln("Content-Id: <" + contentId+">", out);
         OutputUtil.writeln("Content-Transfer-Encoding: binary", out);
         OutputUtil.writeln(out);
-    }
-
-    private void writeMtomBinary(DataHandler dataHandler){
-        throw new UnsupportedOperationException();
     }
 
     private void writeAttachments(AttachmentSet attachments, OutputStream out) throws IOException {
@@ -231,15 +206,6 @@ public class MtomCodec extends MimeCodec {
 
     public MtomCodec copy() {
         return new MtomCodec(version);
-    }
-
-    public XMLStreamWriterEx getXmlStreamWriterEx() {
-        return xmlStreamWriterEx;
-    }
-
-    public XMLStreamWriterEx getXmlStreamWriterEx(OutputStream os) {
-        this.writer = XMLStreamWriterFactory.createXMLStreamWriter(os);
-        return xmlStreamWriterEx;
     }
 
     private String encodeCid(String ns){
@@ -273,17 +239,32 @@ public class MtomCodec extends MimeCodec {
         )));
     }
 
-    private class MtomStreamWriter implements XMLStreamWriterEx {
+    private class MtomStreamWriter extends XMLStreamWriterFilter implements XMLStreamWriterEx {
+        public MtomStreamWriter(XMLStreamWriter w) {
+            super(w);
+        }
+
         public void writeBinary(byte[] data, int start, int len, String contentType) throws XMLStreamException {
-            writeMtomBinary(data, start, len, contentType);
+            try {
+                ByteArrayBuffer bos = new ByteArrayBuffer(data, start, len, contentType);
+                mtomAttachmentStream.add(bos);
+
+                //write out the xop reference
+                writer.writeCharacters("");
+                osWriter.write(XOP_PREF +bos.contentId+XOP_SUFF);
+            } catch (IOException e) {
+                throw new WebServiceException(e);
+            } catch (XMLStreamException e) {
+                throw new WebServiceException(e);
+            }
         }
 
         public void writeBinary(DataHandler dataHandler) throws XMLStreamException {
-            writeMtomBinary(dataHandler);
+            throw new UnsupportedOperationException();
         }
 
         public OutputStream writeBinary(String contentType) throws XMLStreamException {
-            return writeMtomBinary(contentType);
+            throw new UnsupportedOperationException();
         }
 
         public void writePCDATA(CharSequence data) throws XMLStreamException {
@@ -319,136 +300,11 @@ public class MtomCodec extends MimeCodec {
             public Iterator getPrefixes(String namespaceURI) {
                 return nsContext.getPrefixes(namespaceURI);
             }
-
-        }
-
-        public void close() throws XMLStreamException {
-            writer.close();
-        }
-
-        public void flush() throws XMLStreamException {
-            writer.flush();
-        }
-
-        public void writeEndDocument() throws XMLStreamException {
-            writer.writeEndDocument();
-        }
-
-        public void writeEndElement() throws XMLStreamException {
-            writer.writeEndElement();
-        }
-
-        public void writeStartDocument() throws XMLStreamException {
-            writer.writeStartDocument();
-        }
-
-        public void writeCharacters(char[] text, int start, int len) throws XMLStreamException {
-            writer.writeCharacters(text, start, len);
-        }
-
-        public void setDefaultNamespace(String uri) throws XMLStreamException {
-            writer.setDefaultNamespace(uri);
-        }
-
-        public void writeCData(String data) throws XMLStreamException {
-            writer.writeCData(data);
-        }
-
-        public void writeCharacters(String text) throws XMLStreamException {
-            writer.writeCharacters(text);
-        }
-
-        public void writeComment(String data) throws XMLStreamException {
-            writer.writeComment(data);
-        }
-
-        public void writeDTD(String dtd) throws XMLStreamException {
-            writer.writeDTD(dtd);
-        }
-
-        public void writeDefaultNamespace(String namespaceURI) throws XMLStreamException {
-            writer.writeDefaultNamespace(namespaceURI);
-        }
-
-        public void writeEmptyElement(String localName) throws XMLStreamException {
-            writer.writeEmptyElement(localName);
-        }
-
-        public void writeEntityRef(String name) throws XMLStreamException {
-            writer.writeEntityRef(name);
-        }
-
-        public void writeProcessingInstruction(String target) throws XMLStreamException {
-            writer.writeProcessingInstruction(target);
-        }
-
-        public void writeStartDocument(String version) throws XMLStreamException {
-            writer.writeStartDocument(version);
-        }
-
-        public void writeStartElement(String localName) throws XMLStreamException {
-            writer.writeStartElement(localName);
         }
 
         public NamespaceContextEx getNamespaceContext() {
             NamespaceContext nsContext = writer.getNamespaceContext();
             return new MtomNamespaceContextEx(nsContext);
-        }
-
-        public void setNamespaceContext(NamespaceContext context) throws XMLStreamException {
-            writer.setNamespaceContext(context);
-        }
-
-        public Object getProperty(String name) throws IllegalArgumentException {
-            return writer.getProperty(name);
-        }
-
-        public String getPrefix(String uri) throws XMLStreamException {
-            return writer.getPrefix(uri);
-        }
-
-        public void setPrefix(String prefix, String uri) throws XMLStreamException {
-            writer.setPrefix(prefix, uri);
-        }
-
-        public void writeAttribute(String localName, String value) throws XMLStreamException {
-            writer.writeAttribute(localName, value);
-        }
-
-        public void writeEmptyElement(String namespaceURI, String localName) throws XMLStreamException {
-            writer.writeEmptyElement(namespaceURI, localName);
-        }
-
-        public void writeNamespace(String prefix, String namespaceURI) throws XMLStreamException {
-            writer.writeNamespace(prefix, namespaceURI);
-        }
-
-        public void writeProcessingInstruction(String target, String data) throws XMLStreamException {
-            writer.writeProcessingInstruction(target, data);
-        }
-
-        public void writeStartDocument(String encoding, String version) throws XMLStreamException {
-            writer.writeStartDocument(encoding, version);
-        }
-
-        public void writeStartElement(String namespaceURI, String localName) throws XMLStreamException {
-            writer.writeStartElement(namespaceURI, localName);
-        }
-
-        public void writeAttribute(String namespaceURI, String localName, String value) throws XMLStreamException {
-            writer.writeAttribute(namespaceURI, localName, value);
-        }
-
-        public void writeEmptyElement(String prefix, String localName, String namespaceURI) throws XMLStreamException {
-            writer.writeEmptyElement(prefix, localName, namespaceURI);
-        }
-
-        public void writeStartElement(String prefix, String localName, String namespaceURI) throws XMLStreamException {
-            writer.writeStartElement(prefix, localName, namespaceURI);
-        }
-
-        public void writeAttribute(String prefix, String namespaceURI, String localName, String value) throws XMLStreamException {
-            writer.writeAttribute(prefix, namespaceURI, localName, value);
         }
     }
 
