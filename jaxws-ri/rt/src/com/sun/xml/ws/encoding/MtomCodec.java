32e4024d4bd41a67eab7ed9b83b6a8ad5c88a3c5
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-06-08 18:01:47 +0000

    Simplified MtomCodec further.

--- jaxws-ri/rt/src/com/sun/xml/ws/encoding/MtomCodec.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/encoding/MtomCodec.java
@@ -32,6 +32,7 @@ import com.sun.xml.ws.api.pipe.ContentType;
 import com.sun.xml.ws.message.stream.StreamAttachment;
 import com.sun.xml.ws.streaming.XMLStreamReaderFactory;
 import com.sun.xml.ws.streaming.XMLStreamWriterFactory;
+import com.sun.xml.ws.util.xml.XMLStreamReaderFilter;
 import org.jvnet.staxex.Base64Data;
 import org.jvnet.staxex.NamespaceContextEx;
 import org.jvnet.staxex.XMLStreamReaderEx;
@@ -451,12 +452,7 @@ public class MtomCodec extends MimeCodec {
         }
     }
 
-    private static class MtomXMLStreamReaderEx implements XMLStreamReaderEx {
-        /**
-         * The underlying {@link XMLStreamReader} that does the parsing of the root part.
-         */
-        private final XMLStreamReader reader;
-
+    private static class MtomXMLStreamReaderEx extends XMLStreamReaderFilter implements XMLStreamReaderEx {
         /**
          * The parser for the outer MIME 'shell'.
          */
@@ -473,8 +469,8 @@ public class MtomCodec extends MimeCodec {
         private char[] base64EncodedText;
 
         public MtomXMLStreamReaderEx(MimeMultipartParser mimeMP, XMLStreamReader reader) {
+            super(reader);
             this.mimeMP = mimeMP;
-            this.reader = reader;
         }
 
         public CharSequence getPCDATA() throws XMLStreamException {
@@ -518,18 +514,6 @@ public class MtomCodec extends MimeCodec {
                 return nsContext.getPrefixes(namespaceURI);
             }
 
-        }
-
-        public int getAttributeCount() {
-            return reader.getAttributeCount();
-        }
-
-        public int getEventType() {
-            return reader.getEventType();
-        }
-
-        public int getNamespaceCount() {
-            return reader.getNamespaceCount();
         }
 
         public int getTextLength() {
@@ -595,50 +579,6 @@ public class MtomCodec extends MimeCodec {
             }
         }
 
-        public int nextTag() throws XMLStreamException {
-            return reader.nextTag();
-        }
-
-        public void close() throws XMLStreamException {
-            reader.close();
-        }
-
-        public boolean hasName() {
-            return reader.hasName();
-        }
-
-        public boolean hasNext() throws XMLStreamException {
-            return reader.hasNext();
-        }
-
-        public boolean hasText() {
-            return reader.hasText();
-        }
-
-        public boolean isCharacters() {
-            return reader.isCharacters();
-        }
-
-        public boolean isEndElement() {
-            return reader.isEndElement();
-        }
-
-        public boolean isStandalone() {
-            return reader.isStandalone();
-        }
-
-        public boolean isStartElement() {
-            return reader.isStartElement();
-        }
-
-        public boolean isWhiteSpace() {
-            return reader.isWhiteSpace();
-        }
-
-        public boolean standaloneSet() {
-            return reader.standaloneSet();
-        }
-
         public char[] getTextCharacters() {
             if (xopReferencePresent) {
                 char[] chars = new char[base64AttData.length()];
@@ -647,10 +587,6 @@ public class MtomCodec extends MimeCodec {
                 return chars;
             }
             return reader.getTextCharacters();
-        }
-
-        public boolean isAttributeSpecified(int index) {
-            return reader.isAttributeSpecified(index);
         }
 
         public int getTextCharacters(int sourceStart, char[] target, int targetStart, int length) throws XMLStreamException {
@@ -695,104 +631,12 @@ public class MtomCodec extends MimeCodec {
             return reader.getTextCharacters(sourceStart, target, targetStart, length);
         }
 
-        public String getCharacterEncodingScheme() {
-            return reader.getCharacterEncodingScheme();
-        }
-
-        public String getElementText() throws XMLStreamException {
-            return reader.getElementText();
-        }
-
-        public String getEncoding() {
-            return reader.getEncoding();
-        }
-
-        public String getLocalName() {
-            return reader.getLocalName();
-        }
-
-        public String getNamespaceURI() {
-            return reader.getNamespaceURI();
-        }
-
-        public String getPIData() {
-            return reader.getPIData();
-        }
-
-        public String getPITarget() {
-            return reader.getPITarget();
-        }
-
-        public String getPrefix() {
-            return reader.getPrefix();
-        }
-
         public String getText() {
             if (xopReferencePresent) {
                 String text =  base64AttData.toString();
                 textLength = text.length();
             }
             return reader.getText();
-        }
-
-        public String getVersion() {
-            return reader.getVersion();
-        }
-
-        public String getAttributeLocalName(int index) {
-            return reader.getAttributeLocalName(index);
-        }
-
-        public String getAttributeNamespace(int index) {
-            return reader.getAttributeNamespace(index);
-        }
-
-        public String getAttributePrefix(int index) {
-            return reader.getAttributePrefix(index);
-        }
-
-        public String getAttributeType(int index) {
-            return reader.getAttributeType(index);
-        }
-
-        public String getAttributeValue(int index) {
-            return reader.getAttributeValue(index);
-        }
-
-        public String getNamespacePrefix(int index) {
-            return reader.getNamespacePrefix(index);
-        }
-
-        public String getNamespaceURI(int index) {
-            return reader.getNamespaceURI(index);
-        }
-
-        public QName getName() {
-            return reader.getName();
-        }
-
-        public QName getAttributeName(int index) {
-            return reader.getAttributeName(index);
-        }
-
-        public Location getLocation() {
-            return reader.getLocation();
-        }
-
-        public Object getProperty(String name) throws IllegalArgumentException {
-            return reader.getProperty(name);
-        }
-
-        public void require(int type, String namespaceURI, String localName) throws XMLStreamException {
-            reader.require(type, namespaceURI, localName);
-        }
-
-        public String getNamespaceURI(String prefix) {
-            return reader.getNamespaceURI(prefix);
-        }
-
-        public String getAttributeValue(String namespaceURI, String localName) {
-            return reader.getAttributeValue(namespaceURI, localName);
         }
     }
 
