e07d36a8642c0d89d1feb673deb217c72cb37686
Ken Walsh <ken.walsh@sun.com>
2006-06-23 16:30:16 +0000

    update for java.net push

--- jaxws-ri/hudson-nightly.sh
+++ jaxws-ri/hudson-nightly.sh
@@ -2,5 +2,5 @@
 #Bundles will be pushed to java.net.
 
 cd jax-ws-sources/jaxws-ri
-rm -rf build
-ant -Dbuild.id=jaxws-rearch-2005-nightly_$BUILD_ID clean generate-weekly-binary generate-weekly-source
+
+ant -Dbuild.id=jaxws-rearch-2005-nightly_$BUILD_ID -DDSTAMP=$BUILD_ID clean generate-weekly-binary generate-weekly-source
