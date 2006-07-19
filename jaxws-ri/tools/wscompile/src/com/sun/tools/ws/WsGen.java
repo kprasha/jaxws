6d830772a4ae6f38e1ef0c03ff760ee314b5b967
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-07-19 22:00:35 +0000

    tweaked the tool integration entry point not to use Invoker. It's fair to assume that if the external environment has managed to load JAX-WS, it should also be able to load tools.jar.

--- jaxws-ri/tools/wscompile/src/com/sun/tools/ws/WsGen.java
+++ jaxws-ri/tools/wscompile/src/com/sun/tools/ws/WsGen.java
@@ -21,16 +21,37 @@
  */
 package com.sun.tools.ws;
 
-/**
- * @author Vivek Pandey
- */
+import com.sun.tools.ws.wscompile.CompileTool;
 
+/**
+ * WsGen tool entry point.
+ * 
+ * @author Vivek Pandey
+ * @author Kohsuke Kawaguchi
+ */
 public class WsGen {
+    /**
+     * CLI entry point. Use {@link Invoker} to
+     * load tools.jar
+     */
     public static void main(String[] args) throws Throwable {
-        System.exit(doMain(args));
+        System.exit(Invoker.main("wsgen",args));
     }
 
+    /**
+     * Entry point for tool integration.
+     *
+     * <p>
+     * This does the same as {@link #main(String[])} except
+     * it doesn't invoke {@link System#exit(int)}. This method
+     * also doesn't play with classloaders. It's the caller's
+     * responsibility to set up the classloader to load all jars
+     * needed to run the tool, including <tt>$JAVA_HOME/lib/tools.jar</tt>
+     *
+     * @return
+     *      0 if the tool runs successfully.
+     */
     public static int doMain(String[] args) throws Throwable {
-        return Invoker.main("wsgen",args);
+        return new CompileTool(System.out,"wsgen").run(args) ? 0 : 1;
     }
 }
--- jaxws-ri/tools/wscompile/src/com/sun/tools/ws/WsImport.java
+++ jaxws-ri/tools/wscompile/src/com/sun/tools/ws/WsImport.java
@@ -21,16 +21,37 @@
  */
 package com.sun.tools.ws;
 
-/**
- * @author Vivek Pandey
- */
+import com.sun.tools.ws.wscompile.CompileTool;
 
+/**
+ * WsImport tool entry point.
+ * 
+ * @author Vivek Pandey
+ * @author Kohsuke Kawaguchi
+ */
 public class WsImport {
+    /**
+     * CLI entry point. Use {@link Invoker} to
+     * load tools.jar
+     */
     public static void main(String[] args) throws Throwable {
-        System.exit(doMain(args));
+        System.exit(Invoker.main("wsimport",args));
     }
 
+    /**
+     * Entry point for tool integration.
+     *
+     * <p>
+     * This does the same as {@link #main(String[])} except
+     * it doesn't invoke {@link System#exit(int)}. This method
+     * also doesn't play with classloaders. It's the caller's
+     * responsibility to set up the classloader to load all jars
+     * needed to run the tool, including <tt>$JAVA_HOME/lib/tools.jar</tt>
+     *
+     * @return
+     *      0 if the tool runs successfully.
+     */
     public static int doMain(String[] args) throws Throwable {
-        return Invoker.main("wsimport",args);
+        return new CompileTool(System.out,"wsimport").run(args) ? 0 : 1;
     }
 }
