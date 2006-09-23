928503aaa4eb4b0b4bfbaa7bc498c6b274419f29
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2006-09-23 16:41:32 +0000

    Adding fronjava-wsaddressing sample. Will add readme and documentation on this soon.

---
+++ jaxws-ri/samples/fromjava-wsaddressing/src/fromjava_wsaddressing/client/AddNumbersClient.java
@@ -0,0 +1,100 @@
+/*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License).  You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the license at
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
+ */
+package fromjava_wsaddressing.client;
+
+public class AddNumbersClient {
+    int number1 = 10;
+    int number2 = 10;
+    int negativeNumber = -10;
+
+    public static void main(String[] args) {
+        AddNumbersClient client = new AddNumbersClient();
+        client.addNumbers();
+        client.addNumbersFault();
+        client.addNumbers2();
+        client.addNumbers3Fault();
+    }
+
+    public void addNumbers() {
+        System.out.printf("addNumbers: basic input and output name mapping\n");
+        try {
+            AddNumbersImpl stub = createStub();
+            int result = stub.addNumbers(number1, number2);
+            assert result == 20;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            assert false;
+        }
+        System.out.printf("\n\n");
+    }
+
+    public void addNumbersFault() {
+        System.out.printf("addNumbersFault: fault caused by out of bounds parameter value\n");
+        AddNumbersImpl stub = null;
+
+        try {
+            stub = createStub();
+            int result = stub.addNumbers(negativeNumber, number2);
+            assert false;
+        } catch (AddNumbersException_Exception ex) {
+        } catch (Exception e) {
+            e.printStackTrace();
+            assert false;
+        }
+        System.out.printf("\n\n");
+    }
+
+    public void addNumbers2() {
+        System.out.printf("addNumbers2: default input and output name mapping\n");
+        try {
+            AddNumbersImpl stub = createStub();
+            int result = stub.addNumbers2(number1, number2);
+            assert result == 20;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            assert false;
+        }
+        System.out.printf("\n\n");
+    }
+
+    public void addNumbers3Fault() {
+        AddNumbersImpl stub = null;
+
+        System.out.printf("addNumbers3Fault: custom fault mapping\n");
+        try {
+            stub = createStub();
+            int result = stub.addNumbers3(negativeNumber, number2);
+            assert false;
+        } catch (AddNumbersException_Exception ex) {
+        } catch (Exception e) {
+            e.printStackTrace();
+            assert false;
+        }
+        System.out.printf("\n\n");
+    }
+
+    private AddNumbersImpl createStub() throws Exception {
+        AddNumbersImpl stub = new AddNumbersImplService().getAddNumbersImplPort();
+
+        return stub;
+    }
+}
---
+++ jaxws-ri/samples/fromjava-wsaddressing/src/fromjava_wsaddressing/server/AddNumbersImpl.java
@@ -0,0 +1,69 @@
+/*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License).  You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the license at
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
+ */
+package fromjava_wsaddressing.server;
+
+import javax.annotation.Resource;
+import javax.jws.WebService;
+import javax.xml.ws.*;
+import javax.xml.ws.soap.SOAPBinding;
+import javax.xml.ws.soap.AddressingFeature;
+
+@WebService
+@BindingType(value = SOAPBinding.SOAP11HTTP_BINDING,
+        features = {@Feature(value = AddressingFeature.ID)})
+
+public class AddNumbersImpl {
+
+    @Resource
+    WebServiceContext wsc;
+
+    @Action(
+            input = "http://example.com/input",
+            output = "http://example.com/output")
+    public int addNumbers(int number1, int number2) throws AddNumbersException {
+        return impl(number1, number2);
+    }
+
+    public int addNumbers2(int number1, int number2) throws AddNumbersException {
+        return impl(number1, number2);
+    }
+
+    @Action(
+            input = "http://example.com/input3",
+            output = "http://example.com/output3",
+            fault = {
+            @FaultAction(className = AddNumbersException.class, value = "http://example.com/fault3")
+                    }
+    )
+    public int addNumbers3(int number1, int number2) throws AddNumbersException {
+        return impl(number1, number2);
+    }
+
+    int impl(int number1, int number2) throws AddNumbersException {
+        if (number1 < 0 || number2 < 0) {
+            throw new AddNumbersException("Negative numbers can't be added!",
+                    "Numbers: " + number1 + ", " + number2);
+        }
+        return number1 + number2;
+    }
+}
+
