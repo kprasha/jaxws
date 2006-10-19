e6a87895bfeb2622afd6711a681d98fcf7a64d15
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2006-10-19 18:22:40 +0000

    Adding javadoc

--- jaxws-ri/rt/src/com/sun/xml/ws/api/pipe/StreamSOAPCodec.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/api/pipe/StreamSOAPCodec.java
@@ -1,15 +1,27 @@
 package com.sun.xml.ws.api.pipe;
 
 import com.sun.xml.ws.api.message.Message;
+import com.sun.istack.NotNull;
 
 import javax.xml.stream.XMLStreamReader;
 
 /**
- * TODO javadoc
+ * Reads events from {@link XMLStreamReader} and constructs a
+ * {@link Message} for SOAP envelope. {@link Codecs} allows a
+ * way to construct a whole codec that can handle MTOM, MIME
+ * encoded packages using this codec.
+ *
+ * @see {@link Codecs}
  * 
  * @author Jitendra Kotamraju
- *
  */
 public interface StreamSOAPCodec extends Codec {
-    public Message decode(XMLStreamReader reader);
+    /**
+     * Reads events from {@link XMLStreamReader} and constructs a
+     * {@link Message} for SOAP envelope.
+     *
+     * @param reader that represents SOAP envelope infoset
+     * @return a {@link Message} for SOAP envelope
+     */
+    public @NotNull Message decode(@NotNull XMLStreamReader reader);
 }
