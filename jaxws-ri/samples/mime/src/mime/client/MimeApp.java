80332b36f0ac60e74cc337ffc50af05e8844d2ef
Vivek Pandey <vivek.pandey@sun.com>
2006-10-21 01:17:23 +0000

    - Removed unnecessary bindings - Additional cleanup

--- jaxws-ri/samples/mime/src/mime/client/MimeApp.java
+++ jaxws-ri/samples/mime/src/mime/client/MimeApp.java
@@ -1,31 +1,4 @@
-/**
- * $Id: MimeApp.java,v 1.4.2.2.2.2 2006-10-16 04:18:13 vivekp Exp $
- */
-
-/*
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the License).  You may not use this file except in
- * compliance with the License.
- *
- * You can obtain a copy of the license at
- * https://glassfish.dev.java.net/public/CDDLv1.0.html.
- * See the License for the specific language governing
- * permissions and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL
- * Header Notice in each file and include the License file
- * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
- * If applicable, add the following below the CDDL Header,
- * with the fields enclosed by brackets [] replaced by
- * you own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
- */
 package mime.client;
-
-import com.sun.xml.ws.util.ASCIIUtility;
 
 import javax.xml.ws.Holder;
 import javax.xml.transform.stream.StreamSource;
@@ -46,12 +19,12 @@ public class MimeApp {
             }
 
             //test wsdl mime without enableMIMEContent(default)
-//            testEchoData ((Hello)port);
+            testEchoData ((Hello)port);
 
             //test wsdl mime with enableMIMEContent
-//            testEchoDataWithEnableMIMEContent((Hello)port);
+            testEchoDataWithEnableMIMEContent((Hello)port);
 
-//            testDetail((Hello)port);
+            testDetail((Hello)port);
 
             //test swaref
             testSwaref ((Hello)port);
--- jaxws-ri/samples/mime/src/mime/server/HelloImpl.java
+++ jaxws-ri/samples/mime/src/mime/server/HelloImpl.java
@@ -1,29 +1,5 @@
-/**
- * $Id: HelloImpl.java,v 1.2.12.1.2.1 2006-08-23 01:04:22 vivekp Exp $
- */
 package mime.server;
 
-/*
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the License).  You may not use this file except in
- * compliance with the License.
- * 
- * You can obtain a copy of the license at
- * https://glassfish.dev.java.net/public/CDDLv1.0.html.
- * See the License for the specific language governing
- * permissions and limitations under the License.
- * 
- * When distributing Covered Code, include this CDDL
- * Header Notice in each file and include the License file
- * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
- * If applicable, add the following below the CDDL Header,
- * with the fields enclosed by brackets [] replaced by
- * you own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
- * 
- * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
- */
 import javax.jws.WebService;
 import javax.xml.ws.Holder;
 import javax.xml.transform.Source;
