e66b2634d2980a51be2ac8798199abe6ced316cf
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-10-30 21:42:17 +0000

    doc improvement.

--- jaxws-ri/rt/src/com/sun/xml/ws/api/model/wsdl/WSDLFeaturedObject.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/api/model/wsdl/WSDLFeaturedObject.java
@@ -3,12 +3,19 @@ package com.sun.xml.ws.api.model.wsdl;
 import com.sun.istack.NotNull;
 import com.sun.istack.Nullable;
 import com.sun.xml.ws.api.WSFeatureList;
+import com.sun.xml.ws.api.wsdl.parser.WSDLParserExtension;
 
+import javax.xml.ws.Dispatch;
 import javax.xml.ws.WebServiceFeature;
 
 /**
- * {@link WSDLObject} that can have features associated.
- * 
+ * {@link WSDLObject} that can have features associated with it.
+ *
+ * <p>
+ * {@link WSDLParserExtension}s can add features to this object,
+ * which will then be incorporated when {@link Dispatch}s and
+ * proxies are created for the port.
+ *
  * @author Kohsuke Kawaguchi
  */
 public interface WSDLFeaturedObject extends WSDLObject {
