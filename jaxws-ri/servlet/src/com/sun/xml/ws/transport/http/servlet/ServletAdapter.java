59a89a80257d3ff0845997b36cffe4ad6f95c161
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-11-03 23:56:26 +0000

    [NEWS:wstx jax-ws] Added additional proposed SPI for communicating HTTP host/port name with the container.

--- jaxws-ri/servlet/src/com/sun/xml/ws/transport/http/servlet/ServletAdapter.java
+++ jaxws-ri/servlet/src/com/sun/xml/ws/transport/http/servlet/ServletAdapter.java
@@ -22,17 +22,24 @@
 
 package com.sun.xml.ws.transport.http.servlet;
 
+import com.sun.istack.NotNull;
 import com.sun.xml.ws.api.model.wsdl.WSDLPort;
-import com.sun.xml.ws.transport.http.WSHTTPConnection;
+import com.sun.xml.ws.api.server.BoundEndpoint;
+import com.sun.xml.ws.api.server.Module;
 import com.sun.xml.ws.api.server.WSEndpoint;
+import com.sun.xml.ws.api.server.WebModule;
 import com.sun.xml.ws.transport.http.HttpAdapter;
-import com.sun.xml.ws.transport.http.HttpAdapterList;
+import com.sun.xml.ws.transport.http.WSHTTPConnection;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.namespace.QName;
+import javax.xml.ws.WebServiceException;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.logging.Logger;
 
 /**
  * {@link HttpAdapter} for servlets.
@@ -41,8 +48,13 @@ import java.io.IOException;
  * This is a thin wrapper around {@link HttpAdapter} with some description
  * specified in the deployment (in particular those information are related
  * to how a request is routed to a {@link ServletAdapter}.
+ *
+ * <p>
+ * This class implements {@link BoundEndpoint} and represent the
+ * servlet-{@link WSEndpoint} association for {@link }
+ *
  */
-public final class ServletAdapter extends HttpAdapter {
+public final class ServletAdapter extends HttpAdapter implements BoundEndpoint {
     final String name;
     /**
      * Servlet URL pattern with which this {@link HttpAdapter} is associated.
@@ -54,6 +66,14 @@ public final class ServletAdapter extends HttpAdapter {
         super(endpoint, owner);
         this.name = name;
         this.urlPattern = urlPattern;
+        // registers itself with the container
+        Module module = endpoint.getContainer().getSPI(Module.class);
+        if(module==null)
+            LOGGER.warning("Container "+endpoint.getContainer()+" doesn't support "+Module.class);
+        else {
+            module.getBoundEndpoints().add(this);
+        }
+
     }
 
     /**
@@ -62,6 +82,23 @@ public final class ServletAdapter extends HttpAdapter {
      */
     public String getName() {
         return name;
+    }
+
+
+    @NotNull
+    public URI getAddress() {
+        WebModule webModule = endpoint.getContainer().getSPI(WebModule.class);
+        if(webModule==null)
+            // this is really a bug in the container implementation
+            throw new WebServiceException("Container "+endpoint.getContainer()+" doesn't support "+WebModule.class);
+
+        String adrs = webModule.getContextPath() + getValidPath();
+        try {
+            return new URI(adrs);
+        } catch (URISyntaxException e) {
+            // this is really a bug in the container implementation
+            throw new WebServiceException("Unable to compute address for "+endpoint,e);
+        }
     }
 
     /**
@@ -121,4 +158,6 @@ public final class ServletAdapter extends HttpAdapter {
 
         return addrBuf.toString();
     }
+
+    private static final Logger LOGGER = Logger.getLogger(ServletAdapter.class.getName());
 }
