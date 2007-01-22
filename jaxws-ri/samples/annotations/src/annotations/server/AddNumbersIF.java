2ebdad907d1df340dfaa61dc4260341756078d68
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2007-01-22 22:55:39 +0000

    WebService  does n't need to extend Remote.

--- jaxws-ri/samples/annotations/src/annotations/server/AddNumbersIF.java
+++ jaxws-ri/samples/annotations/src/annotations/server/AddNumbersIF.java
@@ -10,12 +10,12 @@ import javax.jws.WebParam;
 
 @WebService(targetNamespace = "http://duke.example.org", name="AddNumbers")
 @SOAPBinding(style=SOAPBinding.Style.RPC, use=SOAPBinding.Use.LITERAL)
-public interface AddNumbersIF extends Remote {
+public interface AddNumbersIF {
     
     @WebMethod(operationName="add", action="urn:addNumbers")
     @WebResult(name="return")
     public int addNumbers(
         @WebParam(name="num1")int number1, 
-        @WebParam(name="num2")int number2) throws RemoteException, AddNumbersException;
+        @WebParam(name="num2")int number2) throws AddNumbersException;
 
 }
