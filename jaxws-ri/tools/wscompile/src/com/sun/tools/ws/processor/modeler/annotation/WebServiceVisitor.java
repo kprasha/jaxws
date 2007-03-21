eed93a8abdeed6d1849119c861c62c8ac27967ee
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2007-03-21 15:08:32 +0000

    Fix for issue 272: Does n't throw error if @WebMethod(exclude=true) is present on a SEI's method.

--- jaxws-ri/tools/wscompile/src/com/sun/tools/ws/processor/modeler/annotation/WebServiceVisitor.java
+++ jaxws-ri/tools/wscompile/src/com/sun/tools/ws/processor/modeler/annotation/WebServiceVisitor.java
@@ -583,6 +583,10 @@ public abstract class WebServiceVisitor extends SimpleDeclarationVisitor impleme
 
     protected boolean isLegalMethod(MethodDeclaration method, TypeDeclaration typeDecl) {
         WebMethod webMethod = method.getAnnotation(WebMethod.class);
+        //SEI cannot have methods with @WebMethod(exclude=true)
+        if (typeDecl instanceof InterfaceDeclaration && webMethod != null && webMethod.exclude())
+            builder.onError(method.getPosition(), WebserviceapMessages.localizableWEBSERVICEAP_INVALID_SEI_ANNOTATION_ELEMENT_EXCLUDE("exclude=true", typeDecl.getQualifiedName(), method.toString()));
+        
         if (hasWebMethods && (webMethod == null))
             return true;
         if (!hasWebMethods && (webMethod !=null) && webMethod.exclude()) {
