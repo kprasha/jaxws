87e4fe023ea311d33f0524a7f7b4c15562a4e117
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2011-03-28 22:07:50 +0000

    Porting rev12244 from jaxws22 br, With a NoOpImpl, Don't even log it.

--- rt/src/com/sun/xml/ws/server/MonitorBase.java
+++ rt/src/com/sun/xml/ws/server/MonitorBase.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -282,7 +282,11 @@ public abstract class MonitorBase {
         try {
             final Object ignored = mom.createRoot(this, name);
             if (ignored != null) {
-                logger.log(Level.FINE, "Metro monitoring rootname successfully set to: " + mom.getObjectName(mom.getRoot()));
+                ObjectName ignoredName = mom.getObjectName(mom.getRoot());
+                // The name is null when the MOM is a NOOP.
+                if (ignoredName != null) {
+                    logger.log(Level.INFO, "Metro monitoring rootname successfully set to: " + ignoredName);
+                }
                 return mom;
             }
             try {
