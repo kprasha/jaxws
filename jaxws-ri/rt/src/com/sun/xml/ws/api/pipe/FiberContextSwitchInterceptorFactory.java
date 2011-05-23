eb48326d23943915fa6862b681b881415da693e4
Ryan Eberhard <ryan.eberhard@oracle.com>
2011-05-23 15:39:37 +0000

    Remove reference to WebLogic class

--- jaxws-ri/rt/src/com/sun/xml/ws/api/pipe/FiberContextSwitchInterceptorFactory.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/api/pipe/FiberContextSwitchInterceptorFactory.java
@@ -38,60 +38,17 @@
  * holder.
  */
 
-package com.sun.xml.ws.client;
-
-import com.sun.xml.ws.api.message.Packet;
-import com.sun.xml.ws.api.pipe.Fiber.CompletionCallback;
-import com.sun.xml.ws.api.pipe.Tube;
-
-import javax.xml.ws.WebServiceException;
+package com.sun.xml.ws.api.pipe;
 
 /**
- * Invokes {@link Tube}line asynchronously for the client's async API(for e.g.: Dispatch#invokeAsync}
- * The concrete classes need to call {@link Stub#processAsync(Packet, RequestContext, CompletionCallback)} in
- * run() method.
+ * Factory for (@link FiberContextSwitchInterceptor} instances
  *
- * @author Jitendra Kotamraju
  */
-public abstract class AsyncInvoker implements Runnable {
-
-    /**
-     * Because of the object instantiation order,
-     * we can't take this as a constructor parameter.
-     */
-    protected AsyncResponseImpl responseImpl;
-    protected boolean nonNullAsyncHandlerGiven;
-    
-    public void setReceiver(AsyncResponseImpl responseImpl) {
-        this.responseImpl = responseImpl;
-    }
-
-  public AsyncResponseImpl getResponseImpl() {
-    return responseImpl;
-  }
-
-  public void setResponseImpl(AsyncResponseImpl responseImpl) {
-    this.responseImpl = responseImpl;
-  }
-
-  public boolean isNonNullAsyncHandlerGiven() {
-    return nonNullAsyncHandlerGiven;
-  }
-
-  public void setNonNullAsyncHandlerGiven(boolean nonNullAsyncHandlerGiven) {
-    this.nonNullAsyncHandlerGiven = nonNullAsyncHandlerGiven;
-  }
-
-  public void run () {
-        try {
-            do_run();
-        }catch(WebServiceException e) {
-            throw e;
-        }catch(Throwable t) {
-            //Wrap it in WebServiceException
-            throw new WebServiceException(t);
-        }
-    }
-
-    public abstract void do_run();
+public interface FiberContextSwitchInterceptorFactory {
+	/**
+	 * Creates {@link FiberContextSwitchInterceptor} instance.
+	 * @return interceptor instance
+	 */
+	public FiberContextSwitchInterceptor create();
 }
+
