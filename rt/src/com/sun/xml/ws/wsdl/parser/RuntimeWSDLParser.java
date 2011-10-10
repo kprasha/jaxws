8581615984328247a84fd9432ec358b787a3a2e4
Deva Sagar <deva.sagar@oracle.com>
2011-10-10 14:15:31 +0000

    Porting change made for bug JAX-WS_1001 in jaxws22 branch to this branch When RuntimeWSDLParser.parse() is called, a Source as well as a WSDL location are passed in. If a valid Source is provided, then the performance hit of going over the network to retrieve the WSDL from the URL can be avoided where possible. Also added a check to make sure that if the parser is null, we will fall back to using the Source as before, to create it.

--- rt/src/com/sun/xml/ws/wsdl/parser/RuntimeWSDLParser.java
+++ rt/src/com/sun/xml/ws/wsdl/parser/RuntimeWSDLParser.java
@@ -330,15 +330,34 @@ public class RuntimeWSDLParser {
             }
         }
         if(parser == null){
-            if(wsdlLoc != null)
-                parser = new Parser(wsdlLoc, createReader(wsdlLoc, serviceClass));
-            else
-                parser = new Parser(wsdlLoc, createReader(wsdlSource));
+        	//If a WSDL source is provided that is known to be readable, then
+        	//prioritize that over the URL - this avoids going over the network
+        	//an additional time if a valid WSDL Source is provided - Deva Sagar 09/20/2011
+        	if (wsdlSource != null && isKnownReadableSource(wsdlSource)) {
+            	parser = new Parser(wsdlLoc, createReader(wsdlSource));
+        	} else if (wsdlLoc != null) {
+        		parser = new Parser(wsdlLoc, createReader(wsdlLoc, serviceClass));
+        	}
+        	
+        	//parser could still be null if isKnownReadableSource returns
+        	//false and wsdlLoc is also null. Fall back to using Source based 
+        	//parser since Source is not null
+        	if (parser == null) {
+        		parser = new Parser(wsdlLoc, createReader(wsdlSource));
+        	}
         }
         return parser;
     }
 
-
+    private boolean isKnownReadableSource(Source wsdlSource) {
+		if (wsdlSource instanceof StreamSource) {
+			return (((StreamSource) wsdlSource).getInputStream() != null ||
+					((StreamSource) wsdlSource).getReader() != null);
+		} else {
+			return false;
+		}
+	}
+    
     private XMLStreamReader createReader(@NotNull Source src) throws XMLStreamException {
         return new TidyXMLStreamReader(SourceReaderFactory.createSourceReader(src, true), null);
     }
