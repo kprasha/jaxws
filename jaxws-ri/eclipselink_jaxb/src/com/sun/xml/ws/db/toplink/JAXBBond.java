3b27f5721f6843d784c9ae8a093d4545e3fb5642
Gary Gordon <gary.gordon@oracle.com>
2011-11-09 18:30:19 +0000

    Updating eclipselink jar to sync with latest in WLS.  Added use of new Eclipselink API to optimize XMLStreamReaderEX in plugin.  Added, but commented use of new API for XMLStreamWriterEx, as new eclipselink jar does not yet contain this.

--- jaxws-ri/eclipselink_jaxb/src/com/sun/xml/ws/db/toplink/JAXBBond.java
+++ jaxws-ri/eclipselink_jaxb/src/com/sun/xml/ws/db/toplink/JAXBBond.java
@@ -72,11 +72,13 @@ import org.eclipse.persistence.jaxb.JAXBMarshaller;
 import org.eclipse.persistence.jaxb.JAXBTypeElement;
 import org.eclipse.persistence.jaxb.JAXBUnmarshaller;
 import org.eclipse.persistence.jaxb.TypeMappingInfo;
+//import org.eclipse.persistence.oxm.record.XMLStreamWriterRecord;
 
 import org.eclipse.persistence.internal.oxm.record.XMLStreamReaderInputSource;
 import org.eclipse.persistence.internal.oxm.record.XMLStreamReaderReader;
 import org.jvnet.staxex.Base64Data;
 import org.jvnet.staxex.XMLStreamReaderEx;
+//import org.jvnet.staxex.XMLStreamWriterEx;
 
 import org.w3c.dom.Node;
 import org.xml.sax.ContentHandler;
@@ -110,34 +112,46 @@ public class JAXBBond<T> implements XMLBridge<T> {
 		return typeInfo;
 	}
 
-	public void marshal(T object, XMLStreamWriter output,
-			AttachmentMarshaller am) throws JAXBException {
-		JAXBMarshaller marshaller = null;
-		try {
-			marshaller = parent.mpool.allocate();
-			marshaller.setAttachmentMarshaller(am);
-			marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_FRAGMENT,
-					true);
-			if (mappingInfo != null) {
-				if (isParameterizedType) {
-					JAXBTypeElement jte = new JAXBTypeElement(
-							mappingInfo.getXmlTagName(), object,
-							(ParameterizedType) mappingInfo.getType());
-					marshaller.marshal(jte, output, mappingInfo);
-				} else {
-					JAXBElement<T> elt = new JAXBElement<T>(
-							mappingInfo.getXmlTagName(),
-							(Class<T>) typeInfo.type, object);
-					marshaller.marshal(elt, output, mappingInfo);
-				}
-			} else
-				marshaller.marshal(object, output);
-		} finally {
-			if (marshaller != null) {
-				marshaller.setAttachmentMarshaller(null);
-				parent.mpool.replace(marshaller);
-			}
-		}
+  public void marshal(T object, XMLStreamWriter output, AttachmentMarshaller am)
+      throws JAXBException {
+    JAXBMarshaller marshaller = null;
+    try {
+      marshaller = parent.mpool.allocate();
+      marshaller.setAttachmentMarshaller(am);
+      marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_FRAGMENT, true);
+      //boolean isEx = (output instanceof XMLStreamWriterEx);
+      if (mappingInfo != null) {
+        if (isParameterizedType) {
+          JAXBTypeElement jte = new JAXBTypeElement(
+              mappingInfo.getXmlTagName(), object,
+              (ParameterizedType) mappingInfo.getType());
+          /*if (isEx)
+            marshaller.marshal(jte, new NewStreamWriterRecord(
+                (XMLStreamWriterEx) output), mappingInfo);
+          else*/
+            marshaller.marshal(jte, output, mappingInfo);
+        } else {
+          JAXBElement<T> elt = new JAXBElement<T>(mappingInfo.getXmlTagName(),
+              (Class<T>) typeInfo.type, object);
+          /*if (isEx)
+            marshaller.marshal(elt, new NewStreamWriterRecord(
+                (XMLStreamWriterEx) output), mappingInfo);
+          else*/
+            marshaller.marshal(elt, output, mappingInfo);
+        }
+      } else {
+        /*if (isEx)
+          marshaller.marshal(object, new NewStreamWriterRecord(
+              (XMLStreamWriterEx) output));
+        else*/
+          marshaller.marshal(object, output);
+      }
+    } finally {
+      if (marshaller != null) {
+        marshaller.setAttachmentMarshaller(null);
+        parent.mpool.replace(marshaller);
+      }
+    }
 	}
 
 	// TODO NamespaceContext nsContext
@@ -413,16 +427,21 @@ public class JAXBBond<T> implements XMLBridge<T> {
         //@Override
         protected void parseCharactersEvent(XMLStreamReader xmlStreamReader)
                 throws SAXException {
-            //super.parseCharactersEvent(xmlStreamReader);
+            // super.parseCharactersEvent(xmlStreamReader);
             
-            CharSequence characters;
+            XMLStreamReaderEx xrex = (XMLStreamReaderEx) xmlStreamReader;
             try {
-                characters = ((XMLStreamReaderEx) xmlStreamReader).getPCDATA();
-            } catch (XMLStreamException e) {
-                throw new SAXException(e);
+                CharSequence characters = xrex.getPCDATA();
+                if (characters instanceof Base64Data) {
+                    contentHandler.characters(characters);
+                } else {
+                    String value = characters.toString();
+                    contentHandler.characters(value.toCharArray(), 0,
+                            value.length());
+                }
+            } catch (Exception ex) {
+                throw new WebServiceException(ex);
             }
-            contentHandler.characters(characters);
-
         }
 
         @Override
@@ -438,4 +457,31 @@ public class JAXBBond<T> implements XMLBridge<T> {
             return null;
         }
     }
+    
+    /*
+     * Enable when added to eclipselink jar.
+    public class NewStreamWriterRecord extends XMLStreamWriterRecord {
+      private XMLStreamWriterEx xsw;
+
+      public NewStreamWriterRecord(XMLStreamWriterEx xsw) {
+          super(xsw);
+          this.xsw = xsw;
+      }
+
+      @Override
+      public void characters(QName schemaType, Object value, String mimeType, boolean isCData) {
+          if (mimeType != null) {
+              if (value instanceof DataHandler) {
+                  try {
+                      xsw.writeBinary((DataHandler) value);
+                  } catch (XMLStreamException e) {
+                      throw new WebServiceException(e);
+                  }
+                  return;
+              }
+          }
+          super.characters(schemaType, value, mimeType, isCData);
+       }
+   }
+   */
 }
